d0d76835fb6c685644a33aa49705b95099bded08
gitmarkx <markarielmaata@gmail.com>
2024-03-14 15:15:05 +0800

    separate object array to file / compress images

--- src/components/home/ProjectSection.vue
+++ src/components/home/ProjectSection.vue
@@ -1,79 +1,7 @@
 <script setup>
-import {ref} from 'vue';
-import ProjectContainer from '../icons/ProjectContainer.vue';
+import ProjectContainer from '../ProjectContainer.vue';
+import { projectList } from '@/data/projectList';
 
-const projectList = ref([
-    {
-        link: 'https://www.hatanaka.jp/',
-        image: 'src/assets/image/projects/hatanaka.png',
-        title: 'Hatanaka'
-    }, 
-    {
-        link: 'https://www.tsuboi.biz/',
-        image: 'src/assets/image/projects/tsuboi.jpg',
-        title: 'Tsuboi Industry'
-    }, 
-    {
-        link: 'https://www.esc-espo.jp/espokids/',
-        image: 'src/assets/image/projects/espokids.png',
-        title: 'Espo Kids'
-    }, 
-    {
-        link: 'https://www.lattorti.com/',
-        image: 'src/assets/image/projects/lattorti.png',
-        title: 'Lattorti'
-    }, 
-    {
-        link: 'https://www.seitou-shizuoka.co.jp/',
-        image: 'src/assets/image/projects/seitou-shizuoka.png',
-        title: 'Seitou Shizouka'
-    }, 
-    {
-        link: 'https://www.hello-pharmacy.com/',
-        image: 'src/assets/image/projects/hello-pharmacy.png',
-        title: 'Hello Pharmacy'
-    }, 
-    {
-        link: 'https://avancesys.co.jp/',
-        image: 'src/assets/image/projects/avancesys.png',
-        title: 'Avance System'
-    }, 
-    {
-        link: 'https://shinei-jyuutaku.jp/',
-        image: 'src/assets/image/projects/shinei.png',
-        title: 'Shinei'
-    }, 
-    {
-        link: 'https://ifprint.jp/',
-        image: 'src/assets/image/projects/ifprint.png',
-        title: 'Ifprint'
-    },  
-    {
-        link: 'https://www.shojudo.com/',
-        image: 'src/assets/image/projects/shojudo.png',
-        title: 'Shojudo'
-    }, 
-    {
-        link: 'https://rakue-reform.com/rakueplus/',
-        image: 'src/assets/image/projects/rakueplus.jpeg',
-        title: 'Rakueplus'
-    }, 
-    {
-        link: 'http://cafe-suchard.com/',
-        image: 'src/assets/image/projects/suchard.jpeg',
-        title: 'Suchard'
-    }, 
-    {
-        link: 'https://artsora.net/',
-        image: 'src/assets/image/projects/artsora.jpeg',
-        title: 'Artsora'
-    }, 
-    {
-        link: 'https://mizu-shinkyu.com/',
-        image: 'src/assets/image/projects/mizu-shinkyu.png',
-        title: 'Mizu-Shinkyu'
-    },
-])
 </script>
 <template>
     <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
@@ -105,7 +33,7 @@ const projectList = ref([
 
 .project-image {
     width: 100%;
-    transition: transform 3s ease;
+    transition: transform 10s ease;
 }
 
 .project-container:hover .project-image {
@@ -122,4 +50,4 @@ const projectList = ref([
     /* Semi-transparent background */
     color: white;
 }
-</style>../icons/ProjectItem.vue
+</style>../icons/ProjectItem.vue../ProjectContainer.vue
---
+++ src/data/projectList.js
@@ -0,0 +1,94 @@
+import {ref} from 'vue';
+
+export const projectList = ref([
+    {
+        link: 'https://www.hatanaka.jp/',
+        image: 'src/assets/image/projects/hatanaka.png',
+        title: 'Hatanaka'
+    }, 
+    {
+        link: 'https://www.tsuboi.biz/',
+        image: 'src/assets/image/projects/tsuboi.jpg',
+        title: 'Tsuboi Industry'
+    }, 
+    {
+        link: 'https://www.esc-espo.jp/espokids/',
+        image: 'src/assets/image/projects/espokids.png',
+        title: 'Espo Kids'
+    }, 
+    {
+        link: 'https://www.lattorti.com/',
+        image: 'src/assets/image/projects/lattorti.png',
+        title: 'Lattorti'
+    }, 
+    {
+        link: 'https://www.seitou-shizuoka.co.jp/',
+        image: 'src/assets/image/projects/seitou-shizuoka.png',
+        title: 'Seitou Shizouka'
+    }, 
+    {
+        link: 'https://www.hello-pharmacy.com/',
+        image: 'src/assets/image/projects/hello-pharmacy.png',
+        title: 'Hello Pharmacy'
+    }, 
+    {
+        link: 'https://avancesys.co.jp/',
+        image: 'src/assets/image/projects/avancesys.png',
+        title: 'Avance System'
+    }, 
+    {
+        link: 'https://shinei-jyuutaku.jp/',
+        image: 'src/assets/image/projects/shinei.png',
+        title: 'Shinei'
+    }, 
+    {
+        link: 'https://ifprint.jp/',
+        image: 'src/assets/image/projects/ifprint.png',
+        title: 'Ifprint'
+    },  
+    {
+        link: 'https://www.shojudo.com/',
+        image: 'src/assets/image/projects/shojudo.png',
+        title: 'Shojudo'
+    }, 
+    {
+        link: 'https://rakue-reform.com/rakueplus/',
+        image: 'src/assets/image/projects/rakueplus.jpeg',
+        title: 'Rakueplus'
+    }, 
+    {
+        link: 'http://cafe-suchard.com/',
+        image: 'src/assets/image/projects/suchard.jpeg',
+        title: 'Suchard'
+    }, 
+    {
+        link: 'https://artsora.net/',
+        image: 'src/assets/image/projects/artsora.jpeg',
+        title: 'Artsora'
+    }, 
+    {
+        link: 'http://kusatsu.funeya.jp/',
+        image: 'src/assets/image/projects/kusatsu.png',
+        title: 'Kusatsu Funeya'
+    }, 
+    {
+        link: 'https://mizu-shinkyu.com/',
+        image: 'src/assets/image/projects/mizu-shinkyu.png',
+        title: 'Mizu-Shinkyu'
+    },
+    {
+        link: 'http://asac.kir.jp/PI_test/evision/',
+        image: 'src/assets/image/projects/miraie.png',
+        title: 'Miraie'
+    },
+    {
+        link: 'http://asac.kir.jp/adlive_web_consulting_test/world/index.html',
+        image: 'src/assets/image/projects/world-corporate.png',
+        title: 'World Corporate'
+    },
+    {
+        link: 'https://reunirgracias.iot-mansion.jp/',
+        image: 'src/assets/image/projects/reunir.png',
+        title: 'Reunir'
+    },
+])
